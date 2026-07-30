93dab5d4cf94ff0face6e3e9b8ff05f4796d0666
indhumathiekambaram10 <indhumathiekambaram10@gmail.com>
2026-07-30 07:39:51 +0530

    new changes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from order_tracking import track_order
 from profile import profile
 from reservation import reserve_table
 
-print("===== WELCOME TO FOODEXPRESS =====")
+print("===== WELCOME TO FOOD =====")
 
 role = login()
 
