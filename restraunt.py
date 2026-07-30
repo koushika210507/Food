e0d56a2ac2a28af7aefd37eb378c818ee64e7acc
Koushika <koushiks6212117@gmail.com>
2026-07-30 08:30:04 +0530

    changes done

--- restraunt.py
+++ restraunt.py
@@ -3,4 +3,4 @@ from order_tracking import order_status
 def restaurant_dashboard():
 
     print("\n===== RESTAURANT DASHBOARD =====")
-    print("Current Order Status is :", order_status)
+    print("Current Order Status :", order_status)
